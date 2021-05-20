df60ac1c7dbc92e35d336f2a40cb45421428728e
RaviKumar7870 <b19191@students.iitmandi.ac.in>
2021-05-20 13:21:26 +0530

    Form working

--- interface/urls.py
+++ interface/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.current_meeting),
+    path('',views.current_meeting, name="home"),
+    path('register/',views.registerPage, name="register"),
+
+    path('login/', views.loginPage, name="login"),  
+	path('logout/', views.logoutUser, name="logout"),
 ]
--- interface/views.py
+++ interface/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+
+from django.shortcuts import render, redirect 
+from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from datetime import datetime
 from .models import Link
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from interface import urls
+from django.contrib import messages
 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def current_meeting(request):
     current_time = datetime.now()
@@ -47,3 +56,38 @@ def current_meeting(request):
         return render(request, 'nomeetings.html')
     else:
         return render(request, 'linktoclick.html', {'obj': current_meeting})
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+    context = {'form':form}
+    return render(request,'register.html',context)
+
+def loginPage(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password =request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'login.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login')
