8fe487736fb9a0f18ec503e898acde36081f5ad1
RaviKumar7870 <b19191@students.iitmandi.ac.in>
2021-05-21 21:54:14 +0530

    deploying

--- geteasy/settings.py
+++ geteasy/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8z@got9rf7iq_ok36lio6s_q#@fivvb-%8ynj9h_^l!wc05a$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mymeetapp.herokuapp.com']
 
 
 # Application definition
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import dj_database_url
+prod_db = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
